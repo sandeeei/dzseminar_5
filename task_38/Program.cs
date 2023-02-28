347d311ed09049cec650b582b74234a559c19257
Sergei <sandeeey@example.com>
2023-02-28 09:48:07 +0300

    task_38

---
+++ task_38/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+// Создание массива
+
+double[] GetBinaryArray(int size)
+{
+    double[] array = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+
+        array[i] = Math.Round(100 * (new Random().NextDouble()), 2);
+
+    }
+    return array;
+}
+Console.WriteLine("Введите размер массива:");
+double[] resultArray = GetBinaryArray(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine($"Массив: [ {String.Join("; ", resultArray)} ]");
+int size = resultArray.Length;
+double max = resultArray[0];
+double min = resultArray[0];
+for (int i = 0; i < size; i++)
+{
+    if (resultArray[i] > max)
+    {
+        max = resultArray[i];
+
+    }
+    else if (resultArray[i] < min)
+    {
+        min = resultArray[i];
+    }
+}
+double sum = max - min;
+Console.WriteLine($"Разница между минимальным и максимальным значением массива =  {sum}");
