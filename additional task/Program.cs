ba5ad57234e21c9aacb22323c16813f6a6c08979
Sergei <sandeeey@example.com>
2023-02-28 10:20:40 +0300

    additional_task

---
+++ additional task/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+// Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+//Заполнение массива
+
+int[] GetBinaryArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+
+        array[i] = new Random().Next(-100, 100);
+
+    }
+    return array;
+}
+Console.WriteLine("Введиде размер массива: ");
+int[] resultArray = GetBinaryArray(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine($"Массив: [ {String.Join("; ", resultArray)} ]");
+
+int size = resultArray.Length;
+int result = 1;
+
+int[] NewArray=new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    if (i==(size-1))
+    {
+        result=resultArray[i];
+        NewArray[i] = result;
+    }
+    else
+    {
+    result = resultArray[i] * resultArray[size - 1];
+    NewArray[i] = result;
+    size--;
+    }
+}
+Console.WriteLine($"Массив: [ {String.Join("; ", NewArray)} ]");
